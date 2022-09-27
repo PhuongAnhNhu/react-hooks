0fad7c2ff0b2f3c6ad7ac9534acd82cdfa93948e
Anna Phuong Anh <phuong.nhu@endava.com>
2022-09-27 15:45:54 +0200

    use useLocalStorageState

--- src/exercise/04.js
+++ src/exercise/04.js
@@ -2,42 +2,28 @@
 // http://localhost:3000/isolated/exercise/04.js
 
 import * as React from 'react'
-import {useState} from 'react'
+import {useLocalStorageState} from '../utils'
 
 function Board() {
-  // 🐨 squares is the state for this component. Add useState for squares
-  const [squares, setSquares] = useState(Array(9).fill(null))
+  const [squares, setSquares] = useLocalStorageState(
+    'squares',
+    Array(9).fill(null),
+  )
 
-  // 🐨 We'll need the following bits of derived state:
   const nextValue = calculateNextValue(squares)
   const winner = calculateWinner(squares)
   const status = calculateStatus(winner, squares, nextValue)
-  // 💰 I've written the calculations for you! So you can use my utilities
-  // below to create these variables
 
-  // This is the function your square click handler will call. `square` should
-  // be an index. So if they click the center square, this will be `4`.
   function selectSquare(square) {
-    // 🐨 first, if there's already winner or there's already a value at the
-    // given square index (like someone clicked a square that's already been
-    // clicked), then return early so we don't make any state changes
     if (winner || squares[square]) {
       return
     }
-    // 🦉 It's typically a bad idea to mutate or directly change state in React.
-    // Doing so can lead to subtle bugs that can easily slip into production.
-    //
-    // 🐨 make a copy of the squares array
     const squaresCopy = [...squares]
 
-    // 🐨 set the value of the square that was selected
     squaresCopy[square] = nextValue
-
-    // 🐨 set the squares to your copy
     setSquares(squaresCopy)
   }
 
-  // 🐨 reset the squares
   function restart() {
     setSquares(Array(9).fill(null))
   }
@@ -52,7 +38,6 @@ function Board() {
 
   return (
     <div>
-      {/* 🐨 put the status in the div below */}
       <div className="status">{status}</div>
       <div className="board-row">
         {renderSquare(0)}
